578ac83630a03956b7a9c01d7b62088b44e5cfb6
George Cordalis <cordalis.g@gmail.com>
2018-11-23 12:52:21 +1300

    further heroku configuration

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,3 +4,6 @@ module.exports = {
   SERVER_URL: '"https://editcuts.herokuapp.com"',
   PORT: '"' + process.env.PORT + '"',
 }
+
+console.log('This should print out the port')
+console.log(process.env.PORT)
