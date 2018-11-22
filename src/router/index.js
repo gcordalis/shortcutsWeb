677ac545da5b4f448d40af03d20a97c73541d5b5
George Cordalis <cordalis.g@gmail.com>
2018-11-23 09:32:17 +1300

    removed original editor + dependencies

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import editor from '@/components/editor'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'editor',
-      component: editor
-    }
+
   ]
 })
